747b499eeeec22b793b9925d3049be6a2e5f77d7
everybitmihaly <misi823@gmail.com>
2024-02-11 15:04:21 +0100

    simple copy

--- results_extraction.py
+++ results_extraction.py
@@ -3,38 +3,63 @@ from selenium.webdriver.common.by import By
 from utils import simple_find_or_none
 
 
-column_main_values = {'Határozat sorszáma': 'sorszam',
+main_columns = {'Határozat sorszáma': 'sorszam',
                       'Bíróság': 'birosag',
                       'Kollégium': 'kollegium',
                       'Jogterület': 'jogterulet',
                       'Év': 'year'}
 
+detail_columns = ['egyedi_azonosito', 'kapcsolodo_hatarozatok', 'jogszabalyhelyek', 'elvi_tartalma']
+detail_column_values = {'Egyedi azonositó': 'egyedi_azonosito',
+                        'Kapcsolódó határozatok': 'kapcsolodo_hatarozatok',
+                        'Jogszabályhelyek': 'jogszabalyhelyek',
+                        'A határozat elvi tartalma': 'elvi_tartalma'}
 
-def get_rows(driver):
-    tbody = driver.find_element(by=By.XPATH, value='//tbody[@id="anonimHatarozatGridContent"]')
-    main_rows = tbody.find_elements(by=By.XPATH, value='/tr[@class="add-border-top"]')
-    detail_rows = tbody.find_elements(by=By.XPATH, value='/tr[not(@class="add-border-top")]')  # not(contains(@title,'短期'))
 
-    print(len(main_rows), len(detail_rows))
+def get_rows(driver) -> list:
 
-    # 1a) Get metadata
-    for row_element in main_rows:
+    rows_data = []
+
+    # tbody = driver.find_element(by=By.XPATH, value='//tbody[@id="anonimHatarozatGridContent"]')
+    main_rows = driver.find_elements(by=By.XPATH, value='//tbody[@id="anonimHatarozatGridContent"]/tr[@class="add-border-top"]')
+    detail_rows = driver.find_elements(by=By.XPATH, value='//tbody[@id="anonimHatarozatGridContent"]/tr[not(@class="add-border-top")]')  # not(contains(@title,'短期'))
+
+    if len(main_rows) != len(detail_rows):  # for self
+        raise NotImplementedError('Not same number of main and detail rows - bad implementation!')
+
+    # Get metadata
+    for row_n, (main_row, detail_row) in enumerate(zip(main_rows, detail_rows), start=1):
+
         row_data = {}
-        # 1a.1) Get main values from row
-        # columns = row_element.find_elements(by=By.XPATH, value='//td')
-        # for column in columns:
-        #     column_title = column.find_element(
-        #         by=By.XPATH, value='//span[@class="d-md-none d-inline-block tableTitle"]').text.strip()
-        #     column_value = column.find_element(by=By.XPATH, value='//span[@class="tableData"]').text.strip()
-        #     row_data[column_main_values[column_title]] = column_value
-        #
-        # # 1a.2) Get detail values from "Részletek" dropdown
-        # reszletek_button = row_element.find_element(by=By.XPATH, value='//a[@class="green-text underlined collapsed"]')
-        # reszletek_button.click()
-        #
-        # # 1b) Save metadata in SQL (with filename)
-        #
-        # # 2a) Download file
+
+        # 1) Get main values from row
+        column_titles = main_row.find_elements(by=By.XPATH, value='.//td/span[@class="d-md-none d-inline-block tableTitle"]')
+        column_values = main_row.find_elements(by=By.XPATH, value='.//td/span[@class="tableData"]')
+        for column_title, column_value in zip(column_titles, column_values):
+            column_title_text = column_title.get_attribute("textContent").strip()
+            column_value_text = column_value.text.strip()
+            row_data[main_columns[column_title_text]] = column_value_text
+
+        # 2) Get detail values from "Részletek" dropdown
+        detail_row_divs = detail_row.find_elements(by=By.XPATH,
+                                                   value='.//div[@class="col-12" and not(@class="col-12 d-md-none")]')
+        for detail_row_div in detail_row_divs:
+            label = detail_row_div.find_element(by=By.TAG_NAME, value='label').get_attribute("textContent").strip()
+            detail_column = detail_column_values.get(label)
+            if detail_column is None:
+                raise ValueError(f'Wrong implementation of detail column sequence! Value {label} not in '
+                                 f'detail_column_values lookup!')
+
+            if detail_column == 'kapcsolodo_hatarozatok':
+                hatarozatok_a_tags = detail_row_div.find_elements(by=By.TAG_NAME, value='a')
+                if len(hatarozatok_a_tags) > 0:
+                    row_data[detail_column] = [(a.text.strip(), a.get_attribute('href')) for a in hatarozatok_a_tags]
+            else:
+                row_data[detail_column] = \
+                    detail_row_div.find_element(by=By.XPATH,
+                                                value='.//div[@class="tableData"]').get_attribute("textContent").strip()
+
+        # 3) Download file
         # download_dropdown_button = row_element.find_element(by=By.TAG_NAME, value='button')
         # download_dropdown_button.click()
         #
@@ -42,5 +67,7 @@ def get_rows(driver):
         # download_button.click()
         #
         # # 2b) Rename downloaded file and move to folder to use
-
-        exit(10)
+        yield row_data
+    #     rows_data.append(row_data)
+    #
+    # return rows_data
